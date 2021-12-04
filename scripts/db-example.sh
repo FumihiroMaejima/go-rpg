bc0f7086b1a461f0728bcd6474a2f3ea5a47fe89
Fumihiro_Maejima <testerwatson1101@gmail.com>
2021-12-05 02:17:22 +0900

    add db-example.

---
+++ scripts/db-example.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+SEPARATOPION='+++++++++++++++++++++++++++'
+START_MESSAGE='controle local db container.'
+# CURRENT=${PWD}
+# プロジェクトごとにパスを調整する
+TARGET=${HOME}/path/local-db
+echo ${SEPARATOPION}
+echo ${START_MESSAGE}
+
+# ls ${HOME}/path
+cd ${TARGET} && make serve
+
+echo ${SEPARATOPION}
+echo 'Please "make db" again if you would start or stop local db!'
